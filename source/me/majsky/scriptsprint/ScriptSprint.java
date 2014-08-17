895fa80a5607161bf54a07a3c49047f15980426f
majsky <majsky@hotmail.com>
2014-08-17 22:07:31 +0200

    first test of luaj

--- source/me/majsky/scriptsprint/ScriptSprint.java
+++ source/me/majsky/scriptsprint/ScriptSprint.java
@@ -1,9 +1,15 @@
 package me.majsky.scriptsprint;
 
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
 public class ScriptSprint {
     
     public static void main(String[] args){
-        
+        Globals globals = JsePlatform.standardGlobals();
+        LuaValue chunk = globals.load(ScriptSprint.class.getResourceAsStream("/main.lua"), "main.lua", "t", globals);
+        chunk.call();
     }
     
 }
